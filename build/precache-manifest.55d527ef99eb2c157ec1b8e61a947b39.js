ef0394cf0a88bf6344f153f8b6dd62f42796a0b2
sumit kumar <sumitg837@gmail.com>
2020-05-28 20:17:30 +0530

    initial commit

---
+++ build/precache-manifest.55d527ef99eb2c157ec1b8e61a947b39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1d2eccbfd18539262b7d8862bca5b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26e4da26f966e9a63039",
+    "url": "/static/css/2.d7abb70a.chunk.css"
+  },
+  {
+    "revision": "534854528566ec4c552b",
+    "url": "/static/css/main.201feac4.chunk.css"
+  },
+  {
+    "revision": "26e4da26f966e9a63039",
+    "url": "/static/js/2.981fbac9.chunk.js"
+  },
+  {
+    "revision": "95aed6a00765aeb803e861408bf7612d",
+    "url": "/static/js/2.981fbac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "534854528566ec4c552b",
+    "url": "/static/js/main.d144a570.chunk.js"
+  },
+  {
+    "revision": "36c882f10f5d3068306f",
+    "url": "/static/js/runtime-main.bd7d7b23.js"
+  }
+]);
